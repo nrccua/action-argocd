d10e057784ce0d7c6ad32ad7e322c02961679189
Carlos Ortiz <carlos.ortiz@nrccua.org>
2021-03-24 09:13:23 -0600

    Added --grpc-web flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NOW="$(date)"
-argocd app patch-resource $APP --server $SERVER --kind Deployment  --patch "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"lastUpdate\":\"$NOW\", \"gitBranch\":\"$COMMIT_ID\", \"commitId\":\"$BRANCH\"}}}}}" --auth-token $TOKEN
+argocd app patch-resource $APP --server $SERVER --kind Deployment  --patch "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"lastUpdate\":\"$NOW\", \"gitBranch\":\"$COMMIT_ID\", \"commitId\":\"$BRANCH\"}}}}}" --auth-token $TOKEN --grpc-web
 
