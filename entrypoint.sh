4999ca6a902080b307f2abe0544d5b48373af276
Carlos Ortiz <carlos.ortiz@nrccua.org>
2021-03-24 09:50:53 -0600

    Fix Date format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-NOW="$(date)"
+NOW="$(date '+%Y-%m-%d_%H-%M-%S_%Z')"
 argocd app patch-resource $APP --server $SERVER --kind Deployment  --patch "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"lastUpdate\":\"$NOW\", \"gitBranch\":\"$COMMIT_ID\", \"commitId\":\"$BRANCH\"}}}}}" --auth-token $TOKEN --grpc-web
 
